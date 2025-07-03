1a7a84357364ca3a590a9170557b22eb5d86754f
bit2swaz <bit2swaz@gmail.com>
2025-07-03 19:16:28 +0530

    add suspense to latestinvoice

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -4,7 +4,10 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 import { Suspense } from "react";
-import { RevenueChartSkeleton } from "@/app/ui/skeletons";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from "@/app/ui/skeletons";
 
 export default async function Page() {
   const latestInvoices = await fetchLatestInvoices();
@@ -33,6 +36,9 @@ export default async function Page() {
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
         </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
